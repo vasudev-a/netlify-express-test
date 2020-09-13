b810d60a55905612d4100b08f4860c68d63c07c5
vasudev_a <vasudevanubrolu@gmail.com>
2020-09-13 22:25:04 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -9,14 +9,6 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177741742-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-177741742-1');
-    </script>
     <h1>Hello from Express.js!</h1>
   );
   res.end();
