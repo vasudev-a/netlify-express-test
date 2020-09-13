20f733bc5b8d297ee47e9eb33b31362ea7f1183b
vasudev_a <vasudevanubrolu@gmail.com>
2020-09-13 22:15:44 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -8,7 +8,17 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write(
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177741742-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-177741742-1');
+    </script>
+    <h1>Hello from Express.js!</h1>
+  );
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
